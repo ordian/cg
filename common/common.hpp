ce9c63721d773c4810ee8db845e2de6231fa7513
Andronik Ordian <andronik.ordian@gmail.com>
2014-09-29 22:18:32 +0400

    extract common

---
+++ common/common.hpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdint>
+
+typedef std::int32_t Int;
+typedef std::int64_t Long;
+
+struct Point {
+  Int x, y;
+
+  Point() : x(0), y(0) {}
+  Point(Int a, Int b) : x(a), y(b) {}
+
+  friend std::istream &operator>>(std::istream &input, Point &p) {
+    input.ignore(256, '(');
+    input >> p.x;
+    input.ignore(256, ',');
+    input >> p.y;
+    input.ignore(256, ')');
+    return input;
+  }
+
+  bool operator<(Point &p) {
+    return (y < p.y) || ((y == p.y) && (x < p.x));
+  }
+};
+
+struct Box {
+  Int minX, maxX;
+  Int minY, maxY;
+
+  void update(Point const &p) {
+    if (empty) {
+      minX = maxX = p.x;
+      minY = maxY = p.y;
+      empty = false;
+    } else {
+      minX = std::min(minX, p.x);
+      minY = std::min(minY, p.y);
+      maxX = std::max(maxX, p.x);
+      maxY = std::max(maxY, p.y);
+    }
+  }
+
+  bool contains(Point const &p) const {
+    return p.x >= minX && p.y >= minY && p.x <= maxX && p.y <= maxY;
+  }
+
+  void clear() {
+    empty = true;
+  }
+
+private:
+  bool empty = true;
+};
+
+struct Polygon {
+  std::vector<Point> const &getPoints() const {
+    return points;
+  }
+
+  Box const &getBounds() const {
+    return bounds;
+  }
+
+  friend std::istream &operator>>(std::istream &input, Polygon &poly);
+
+private:
+  std::vector<Point> points;
+  Box bounds;
+};
+
+std::istream &operator>>(std::istream &input, Polygon &poly) {
+  size_t n = 0;
+  input >> n;
+  poly.points.clear();
+  poly.bounds.clear();
+  poly.points.resize(n);
+
+  for (size_t i = 0; i != n; ++i) {
+    input >> poly.points[i];
+    poly.bounds.update(poly.points[i]);
+  }
+
+  return input;
+}
+
+enum Orientation {
+  LEFT,
+  COLLINEAR,
+  RIGHT
+};
+
+Long det(Long a, Long b, Long c, Long d) {
+  return a * d - b * c;
+}
+
+Orientation orientation(Point const &p, Point const &q, Point const &r) {
+  Long x = det(
+    (q.x - p.x), (r.x - p.x),
+    (q.y - p.y), (r.y - p.y));
+  return x < 0 ? LEFT : x > 0 ? RIGHT : COLLINEAR;
+}
+
--- task01/main.cpp
+++ task01/main.cpp
@@ -1,94 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstdint>
-
-typedef std::int32_t Int;
-typedef std::int64_t Long;
-
-struct Point {
-  Int x, y;
-
-  Point() : x(0), y(0) {}
-  Point(Int a, Int b) : x(a), y(b) {}
-
-  friend std::istream &operator>>(std::istream &input, Point &p) {
-    input.ignore(256, '(');
-    input >> p.x;
-    input.ignore(256, ',');
-    input >> p.y;
-    input.ignore(256, ')');
-    return input;
-  }
-
-  bool operator<(Point &p) {
-    return (y < p.y) || ((y == p.y) && (x < p.x));
-  }
-};
-
-struct Box {
-  Int minX, maxX;
-  Int minY, maxY;
-
-  void update(Point const &p) {
-    if (empty) {
-      minX = maxX = p.x;
-      minY = maxY = p.y;
-      empty = false;
-    } else {
-      minX = std::min(minX, p.x);
-      minY = std::min(minY, p.y);
-      maxX = std::max(maxX, p.x);
-      maxY = std::max(maxY, p.y);
-    }
-  }
-
-  bool contains(Point const &p) const {
-    return p.x >= minX && p.y >= minY && p.x <= maxX && p.y <= maxY;
-  }
-
-  void clear() {
-    empty = true;
-  }
-
-private:
-  bool empty = true;
-};
-
-enum Orientation {
-  LEFT,
-  COLLINEAR,
-  RIGHT
-};
-
-Long det(Long a, Long b, Long c, Long d) {
-  return a * d - b * c;
-}
-
-Orientation orientation(Point const &p, Point const &q, Point const &r) {
-  Long x = det(
-    (q.x - p.x), (r.x - p.x),
-    (q.y - p.y), (r.y - p.y));
-  return x < 0 ? LEFT : x > 0 ? RIGHT : COLLINEAR;
-}
-
-struct Polygon {
-  bool contains(Point const &point) const;
-
-  std::vector<Point> const &getPoints() const {
-    return points;
-  }
-
-  Box const &getBounds() const {
-    return bounds;
-  }
-
-  friend std::istream &operator>>(std::istream &input, Polygon &poly);
-
-private:
-  std::vector<Point> points;
-  Box bounds;
-};
+#include "../common/common.hpp"
 
 enum SegmentRayIntersection {
   YES,
@@ -121,11 +31,12 @@ segmentHRayIntersection(Point a, Point b, Point const &r) {
   return NO;
 }
 
-bool Polygon::contains(Point const &p) const {
-  if (!bounds.contains(p))
+bool contains(Polygon const &poly, Point const &p) {
+  if (!poly.getBounds().contains(p))
     return false;
 
   bool c = false, skip = false;
+  std::vector<Point> const &points = poly.getPoints();
   for (size_t i = 0, size = points.size(), j = size - 1; i != size; ) {
     Point const &a = points[j], &b = points[i];
     switch (segmentHRayIntersection(skip ? points[i - 1] : a, b, p)) {
@@ -150,23 +61,6 @@ bool Polygon::contains(Point const &p) const {
   return c;
 }
 
-
-std::istream &operator>>(std::istream &input, Polygon &poly) {
-  size_t n = 0;
-  input >> n;
-  poly.points.clear();
-  poly.bounds.clear();
-  poly.points.resize(n);
-
-  for (size_t i = 0; i != n; ++i) {
-    input >> poly.points[i];
-    poly.bounds.update(poly.points[i]);
-  }
-
-  return input;
-}
-
-
 int main() {
   std::ios::sync_with_stdio(false);
   Polygon poly;
@@ -177,7 +71,7 @@ int main() {
   Point p;
   for (size_t i = 0; i != m; ++i) {
     std::cin >> p;
-    std::cout << (poly.contains(p) ? "yes" : "no") << std::endl;
+    std::cout << (contains(poly, p) ? "yes" : "no") << std::endl;
   }
 
   return 0;
